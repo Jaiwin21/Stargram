1c34095176dcc89c13c26bcca899981cb657d423
Jaiwin21 <Jaiwinprince15@gmail.com>
2024-06-30 23:38:16 +0100

    fixed import bug and begun routing pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
 import './global.css';
+import { Routes, Route } from 'react-router-dom';
+import SignInForm from './_auth/forms/SignInForm';
+import SignUpForm from './_auth/forms/SignUpForm';
+import { Home } from './_root/pages';
+import AuthLayout from './_auth/AuthLayout'
+import RootLayout from './_root/RootLayout';
 
 const App = () => {
   return (
-    <h1 className='text-3xl font-bold underline'>
-        Test
-    </h1>
+    <main className='flex h-screen'>
+        <Routes>
+
+            <Route element={<AuthLayout />}>
+                {/* public routes */}
+                <Route path='/sign-in' element={<SignInForm />} />
+                <Route path='/sign-up' element={<SignUpForm />} />
+            </Route>
+
+            <Route element={<RootLayout />}>
+                {/* private route */}
+                <Route index element={<Home />}/>
+            </Route>
+
+        </Routes>
+
+    </main>
   )
 }
 
