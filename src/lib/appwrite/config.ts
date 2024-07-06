d4051d7e32194d54d78cbdccbfd03ec63bc6eb1d
Jaiwin21 <Jaiwinprince15@gmail.com>
2024-07-06 22:46:23 +0100

    vite env and config

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -1,5 +1,7 @@
 import {Client, Account, Databases, Storage, Avatars } from 'appwrite'
 
 export const appwriteConfig = {
-    projectId: '6689b61c001da3013a82'
+    projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID
 }
+
+export const client = new Client ();
