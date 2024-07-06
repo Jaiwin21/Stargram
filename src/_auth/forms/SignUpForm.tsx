02030592511430d9f84ca95774c353e017fab6bc
Jaiwin21 <Jaiwinprince15@gmail.com>
2024-07-06 23:18:03 +0100

    environment appwrite ID's

--- src/_auth/forms/SignUpForm.tsx
+++ src/_auth/forms/SignUpForm.tsx
@@ -15,6 +15,7 @@ import { Input } from "../../components/ui/input";
 import { SignupValidation } from "../../lib/validation";
 import { z } from "zod";
 import Loader from "../../components/shared/Loader";
+import { createUserAccount } from "../../lib/appwrite/api";
 
 const SignUpForm = () => {
 
@@ -33,9 +34,9 @@ const SignUpForm = () => {
 
   // Submit handler
   async function onSubmit(values: z.infer<typeof SignupValidation>) {
-    // const newUser = await createUserAccount(values);
+     const newUser = await createUserAccount(values);
     
-    console.log(values);
+    console.log(newUser);
   }
 
   return (
