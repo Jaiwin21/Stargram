1ec61bab6e3dc36a9a2f6c3ddc966370c7f9dc0e
Jaiwin21 <Jaiwinprince15@gmail.com>
2024-07-08 17:25:30 +0100

    sign in file

--- src/_auth/forms/SignInForm.tsx
+++ src/_auth/forms/SignInForm.tsx
@@ -1,9 +1,124 @@
-import React from 'react'
+import { Button } from "../../components/ui/button";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../../components/ui/form";
+import { Input } from "../../components/ui/input";
+import { SigninValidation } from "../../lib/validation";
+import { z } from "zod";
+import Loader from "../../components/shared/Loader";
+import { useToast } from "../../components/ui/use-toast";
+import { useSignInAccount } from "../../lib/react-query/queriesAndMutations";
+import { useUserContext } from "../../context/AuthContext";
+
 
 const SignInForm = () => {
-  return (
-    <div>SignInForm</div>
-  )
-}
 
-export default SignInForm
+  const { toast } = useToast();
+  const { checkAuthUser, isLoading: isUserLoading } = useUserContext();
+  const navigate = useNavigate();
+  const { mutateAsync: signInAccount } = useSignInAccount();
+
+  // Define form
+  const form = useForm<z.infer<typeof SigninValidation>>({
+    resolver: zodResolver(SigninValidation),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  // Submit handler
+  async function onSubmit(values: z.infer<typeof SigninValidation>) {
+    const session = await signInAccount({
+      email: values.email,
+      password: values.password,
+    })
+
+    if (!session) {
+      return toast ({title: 'Sign in failed. Please try again.'})
+    }
+    const isLoggedIn = await checkAuthUser();
+
+    if(isLoggedIn) {
+      form.reset();
+
+      navigate('/')
+    } else {
+      return toast({title: 'Sign up failed. Please try again.'})
+    }
+  }
+
+  return (
+    // Sign-in form
+    <div className="container">
+      <Form {...form}>
+        <div className="form-wrapper">
+          <img src="/assets/images/star-logo.png" alt="logo" className="logo" />
+          <h2 className="title">Log into your account</h2>
+          <p className="subtitle">
+            Welcome back! Please enter your details.
+          </p>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="form-content"
+          >
+            
+            {/* Email field */}
+              <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input type="email" className="shad-input" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            {/* Password field */}
+              <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" className="shad-input" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+           
+            {/* Button with loading feature */}
+            <Button type="submit" className="shad-button_primary">
+              {isUserLoading ? (
+                <div className="flex-center gap-2">
+                 <Loader /> Loading...
+                </div>
+              ): "Sign in"}
+            </Button>
+            
+            <p className="text-small-regular text-light-2 text-center -mt-2">
+              Don't have an account?
+              <Link to="/sign-up" className="text-primary-300 text-small-semibold ml-1">Sign up</Link>
+            </p>
+
+          </form>
+        </div>
+      </Form>
+    </div>
+  );
+};
+
+export default SignInForm;
