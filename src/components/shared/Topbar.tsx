e91be3b6c6b1f8ff42f7356261e5fcbcbb6fbcb8
Jaiwin21 <Jaiwinprince15@gmail.com>
2024-07-11 23:44:58 +0100

    redone logo and work on homepage

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button } from '../ui/button'
 
 const Topbar = () => {
   return (
-    <div>Topbar</div>
+    <section className='topbar'>
+      <div className='flex-between py-4 px-5'>
+        <Link to="/" className="flex gap-3 items-center">
+          <img 
+            src='/assets/images/stargram-logo.png'
+            alt='logo'
+            width={130}
+            height={325}
+          />
+        </Link>
+
+        <div>
+          <Button className='flex gap-4'>
+            <img src='/assets/icons/logout.svg' alt='logout'/>
+          </Button>
+        </div>
+
+
+      </div>
+    </section>
   )
 }
 
