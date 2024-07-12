9137157275afc9fa8bcc7c1417d76f44c1f668e5
Jaiwin21 <Jaiwinprince15@gmail.com>
2024-07-12 23:15:09 +0100

    side bar profile image

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,6 +1,19 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { Button } from '../ui/button'
+import { useSignOutAccount } from '../../lib/react-query/queriesAndMutations'
+import { useEffect } from 'react';
+import { useUserContext } from '../../context/AuthContext';
 
 const LeftSidebar = () => {
+ 
+ const { mutate: signOut, isSuccess } = useSignOutAccount();
+ const navigate = useNavigate();
+ const { user } = useUserContext();
+
+ useEffect (() => {
+  if (isSuccess) navigate (0);
+ }, [isSuccess])
+
   return (
     <nav className='leftsidebar'>
       <div className='flex flex-col gap-11'>
@@ -12,6 +25,15 @@ const LeftSidebar = () => {
             height={36}
           />
         </Link>
+        
+        <Link to={`/profile/${user.id}`}
+        className='flex gap-3 items-center'>
+          <img 
+          src={user.imageUrl || "/assets/icons/profile-placeholder.svg"} alt="profile"
+          className='h-14 w-14 rounded-full'
+          />
+        </Link>
+
       </div>
     </nav>
   )
