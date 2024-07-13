78d9cd6cb87a7d8e1b6f93c453c1740c658a62b9
Jaiwin21 <Jaiwinprince15@gmail.com>
2024-07-13 17:01:21 +0100

    mobile view bottom bar navigation implementation

--- src/components/shared/Bottombar.tsx
+++ src/components/shared/Bottombar.tsx
@@ -10,20 +10,22 @@ const Bottombar = () => {
     {bottombarLinks.map((link) => {
       const isActive = pathname === link.route;
       return (
-        <li key={link.label}
-        className={`leftsidebar-link group ${isActive && 'bg-primary-500'}`}>
+      
         <Link
           to={link.route}
-          className="flex gap-4 items-center p-4"
+          key={link.label}
+          className={`${isActive && 'bg-primary-500 rounded-[10px]'} flex-center flex-col gap-1 p-2 transition`}
         >
         <img 
           src={link.imgURL} 
-          alt={link.label} 
-          className={`group-hover:invert-white ${isActive && 'invert-white'}`}
+          alt={link.label}
+          width={16}
+          height={16} 
+          className={`${isActive && 'invert-white'}`}
           />
-          {link.label}
+          <p className="tiny-medium text-white-2">{link.label}</p>
         </Link>
-        </li>
+        
       )
     })}
     </section>
