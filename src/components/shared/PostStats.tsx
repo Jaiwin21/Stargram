3b4d1e7dcd0be3e537fd2bcd138f9f5f6aec362c
Jaiwin21 <Jaiwinprince15@gmail.com>
2024-07-15 23:36:55 +0100

    check if post is liked + implementation

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -1,4 +1,8 @@
 import { Models } from "appwrite"
+import { useDeleteSavedPost, useLikePost, useSavePost } from "../../lib/react-query/queriesAndMutations";
+import { useUserContext } from "../../context/AuthContext";
+import { useState } from "react";
+import { checkIsLiked } from "../../lib/utils";
 
 type PostStatsProps = {
     post: Models.Document;
@@ -6,11 +10,30 @@ type PostStatsProps = {
 }
 
 const PostStats = ({ post, userId }: PostStatsProps) => {
-  return (
+  
+  const likesList = post.likes.map((user: Models.Document) => user.$id) 
+  const [likes, setLikes] = useState(likesList);
+  const [isSaved, setIsSaved] = useState(false);
+
+  const { mutate: likePost } = useLikePost();
+  const { mutate: savePost } = useSavePost();
+  const { mutate: deleteSavedPost } = useDeleteSavedPost();
+  
+  const { date: currentUser } = useUserContext();
+
+  const handleLikePost = () => {}
+  const handleSavePost = () => {}
+
+
+
+    return (
     <div className="flex justify-between items-center z-20">
         <div className="flex gap-2 mr-5">
             <img 
-            src="/assets/icons/like.svg" 
+            src={`${checkIsLiked(likes, userId) 
+                ? "/assets/icons/like.svg" 
+                : "/assets/icons/like.svg"
+                }` 
             alt="like"
             width={20}
             height={20}
