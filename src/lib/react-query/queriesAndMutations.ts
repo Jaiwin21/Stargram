b8274536ee5187339cff1bcdaa733def162c22e4
Jaiwin21 <Jaiwinprince15@gmail.com>
2024-07-16 00:07:08 +0100

    like and save functionality implemented

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -4,7 +4,7 @@ import {
     useQueryClient,
     useInfiniteQuery
 } from '@tanstack/react-query';
-import { createPost, createUserAccount, deleteSavePost, getRecentPosts, likePost, savePost, signInAccount, signOutAccount } from '../appwrite/api';
+import { createPost, createUserAccount, deleteSavePost, getCurrentUser, getRecentPosts, likePost, savePost, signInAccount, signOutAccount } from '../appwrite/api';
 import { INewPost, INewUser } from '../../types';
 import { QUERY_KEYS } from './queryKeys';
 import { CreatePost } from '../../_root/pages';
@@ -121,5 +121,12 @@ export const useDeleteSavedPost = () => {
                 queryKey: [QUERY_KEYS.GET_CURRENT_USER]
             })
         }
+    })
+}
+
+export const useGetCurrentUser = () => {
+    return useQuery ({
+        queryKey: [QUERY_KEYS.GET_CURRENT_USER],
+        queryFn: getCurrentUser
     })
 }
