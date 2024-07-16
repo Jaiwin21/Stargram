4039bf2a8e5c6ad88de1b4ed7eaea0bdb48234ec
Jaiwin21 <Jaiwinprince15@gmail.com>
2024-07-16 22:35:56 +0100

    showcasing post stats on post details

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -55,7 +55,7 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
         setIsSaved(false);
         deleteSavedPost(savedPostRecord.$id);
     } else {
-        savePost({ postId: post.$id, userId });
+        savePost({ postId: post?.$id || '', userId });
         setIsSaved(true);
     }
 
