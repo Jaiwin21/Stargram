a0172f550f38eb1e68182633e7046f323f9487f6
Jaiwin21 <Jaiwinprince15@gmail.com>
2024-07-16 22:34:38 +0100

    styling and implementing

--- src/_root/pages/PostDetails.tsx
+++ src/_root/pages/PostDetails.tsx
@@ -2,12 +2,18 @@ import { Link, useParams } from "react-router-dom"
 import { useGetPostById } from "../../lib/react-query/queriesAndMutations"
 import Loader from "../../components/shared/Loader";
 import { formatDateString, multiFormatDateString } from "../../lib/utils";
+import { useUserContext } from "../../context/AuthContext";
+import { Button } from "../../components/ui/button";
+import PostStats from "../../components/shared/PostStats";
 
 
 const PostDetails = () => {
   
   const { id } = useParams();
   const { data: post, isPending } = useGetPostById(id || '');
+  const { user }  = useUserContext();
+
+  const handleDeletePost = () => {}
 
   return (
     <div className="post_details-container">
@@ -21,12 +27,12 @@ const PostDetails = () => {
           <div className="post_details-info">
             <div className="flex-between w-full">
 
-            </div>
+            
             <Link to={`/profile/${post?.creator.$id}`} className="flex items-center gap-3">
               <img 
                 src={post?.creator?.imageUrl}
                 alt="creator"
-                className="rounded-full w-12 lg:h-12" 
+                className="rounded-full w-8 h-8 lg:w-12 lg:h-12" 
               />
 
 
@@ -42,11 +48,57 @@ const PostDetails = () => {
                 <p className="subtle-semibold lg:small-regular">
                     {post?.location}
                 </p>
-            </div>
-            </div>
-            </Link>
           </div>
+          
+          </div>
+            </Link>
+
+            <div className="flex-center">
+              <Link to={`/update-post/${post?.$id}`} className={`${user.id !== post?.creator.$id && 'hidden'}`}>
+              <img 
+              src="/assets/icons/edit.svg" 
+              width={24}
+              height={24}
+              alt="edit" 
+              
+              />
+              </Link>
+
+              <Button
+                onClick={handleDeletePost}
+                variant="ghost"
+                className={`ghost_details-delete_btn ${user.id !== post?.creator.$id && 'hidden'}`}
+              >
+                <img 
+                src="/assets/icons/delete.svg" 
+                alt="delete"
+                width={24}
+                height={24} 
+                />
+
+              </Button>
+
+            </div>
+          </div>
+
+          <hr className="border w-full border-dark-4/80"/>
+
+          <div className="flex flex-col flex-1 w-full small-medium lg:base-regular">
+                <p>{post?.caption}</p>
+                <ul className="flex gap-1 mt-2">
+                    {post?.tags.map((tag: string) => (
+                        <li key={tag} className="text-light-3">
+                            #{tag}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+              <div className="w-full">
+                  <PostStats post={post} userId={user.id}/>
+              </div>
+          </div>  
         </div>
+        
       )}
     </div>
   )
--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -6,13 +6,13 @@ import { checkIsLiked } from "../../lib/utils";
 import Loader from "./Loader";
 
 type PostStatsProps = {
-    post: Models.Document;
+    post?: Models.Document;
     userId: string;
 }
 
 const PostStats = ({ post, userId }: PostStatsProps) => {
   
-  const likesList = post.likes.map((user: Models.Document) => user.$id) 
+  const likesList = post?.likes.map((user: Models.Document) => user.$id) 
   const [likes, setLikes] = useState(likesList);
   const [isSaved, setIsSaved] = useState(false);
 
@@ -22,7 +22,7 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
   
   const { data: currentUser } = useGetCurrentUser();
 
-  const savedPostRecord = currentUser?.save.find((record: Models.Document) => record.post.$id === post.$id);
+  const savedPostRecord = currentUser?.save.find((record: Models.Document) => record.post.$id === post?.$id);
 
   useEffect(() => {
     setIsSaved(!!savedPostRecord)
@@ -41,7 +41,7 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
     }
 
     setLikes(newLikes);
-    likePost({ postId: post.$id, likesArray: newLikes });
+    likePost({ postId: post?.$id || '', likesArray: newLikes });
   }
   
   
