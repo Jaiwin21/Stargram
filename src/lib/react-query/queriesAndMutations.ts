6d53eecfe4711d97055f00da2bc1ac90e3414262
Jaiwin21 <Jaiwinprince15@gmail.com>
2024-07-17 00:55:27 +0100

    bug fixes

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -171,13 +171,13 @@ export const useGetPosts = () => {
         queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
         queryFn: getInfinitePosts,
         getNextPageParam: (lastPage) => {
-            if(lastPage && lastPage.length === 0) return null;
-                const lastId = lastPage.documents[lastPage?.documents.length - 1].$id;
-                return lastId;
-            
-        }
-    })
-}
+            if (!lastPage || !lastPage.documents || lastPage.documents.length === 0) return null;
+            const lastId = lastPage.documents[lastPage.documents.length - 1].$id;
+            return lastId;
+        },
+    });
+};
+
 
 export const useSearchPosts = (searchTeam: string) => {
     return useQuery ({
